1439ac1e67d4dce7a0542e14bee3e07f0eb95a57
maki-engineer <ryohei-81@outlook.jp>
2024-01-30 23:38:52 +0900

    update: 型の追加、フォーマットの修正

--- tests/function/HankakuToZenkaku.test.ts
+++ tests/function/HankakuToZenkaku.test.ts
@@ -1,13 +1,13 @@
 const { zenkakuToHankaku } = require('../../src/function');
 
-describe('正常系（hankakuToZenkaku）', () => {
-  test('全角英数字が半角英数字に変換されること', () => {
+describe('正常系（hankakuToZenkaku）', (): void => {
+  test('全角英数字が半角英数字に変換されること', (): void => {
     const result: string = zenkakuToHankaku('ａｂｃ１２３');
 
     expect(result).toBe('abc123');
   });
 
-  test('全角英数字と半角英数字が混ざっていた場合は、全角英数字の部分のみが変換されること', () => {
+  test('全角英数字と半角英数字が混ざっていた場合は、全角英数字の部分のみが変換されること', (): void => {
     const result: string = zenkakuToHankaku('ａbｃ１2３');
 
     expect(result).toBe('abc123');
@@ -17,8 +17,8 @@ describe('正常系（hankakuToZenkaku）', () => {
     ['半角英数字のみが渡された場合、何も変換されないこと', 'abc123'],
     ['空白の文字列が渡された場合、何も変換されないこと', ' '],
     ['空の文字列が渡された場合、何も変換されないこと', ''],
-  ])('%s', (conditions: string, receivedAndExpected: string) => {
-    const result : string = zenkakuToHankaku(receivedAndExpected);
+  ])('%s', (conditions: string, receivedAndExpected: string): void => {
+    const result: string = zenkakuToHankaku(receivedAndExpected);
 
     expect(result).toBe(receivedAndExpected);
   });
