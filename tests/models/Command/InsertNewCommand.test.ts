4622ac4e005acbb2b985e7ea28580c6f84583339
maki-engineer <ryohei-81@outlook.jp>
2024-01-30 23:34:22 +0900

    add: Commandモデルのユニットテストを実装

---
+++ tests/models/Command/InsertNewCommand.test.ts
@@ -0,0 +1,50 @@
+process.env.NODE_ENV = 'unittest';
+
+var { Command, sequelize } = require('../../../models/index');
+
+
+describe('正常系（Command.insertNewCommand）', (): void => {
+    let transaction: any;
+  
+    beforeEach(async (): Promise < void > => {
+      transaction = await sequelize.transaction();
+    });
+  
+    afterEach(async (): Promise < void > => {
+      if (transaction) {
+        await transaction.rollback();
+      }
+    });
+  
+    afterAll(async (): Promise < void > => {
+      await sequelize.close();
+    });
+
+    test('指定されたコマンド名とコマンド名の詳細が正常に commands テーブルに追加された場合は、オブジェクトが返ること', async (): Promise < void > => {
+      const newCommandName: string = '235ap';
+      const newDescription: string = 'このコマンドは指定された曲をAP曲リストに追加するためのコマンドです。';
+
+      let commandData: [] | { name: string; description: string }[] = await Command.findAll({
+        raw: true,
+        transaction: transaction
+      });
+
+      expect(commandData).toHaveLength(0);
+
+      const result: { name: string; description: string } = await Command.insertNewCommand(
+        newCommandName,
+        newDescription,
+        transaction
+      );
+
+      commandData = await Command.findAll({
+        raw: true,
+        transaction: transaction
+      });
+
+      expect(commandData).toHaveLength(1);
+      expect(result).toBeInstanceOf(Object);
+      expect(commandData[0].name).toBe(result.name);
+      expect(commandData[0].description).toBe(result.description);
+    });
+  });
