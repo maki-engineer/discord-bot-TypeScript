7951b354ab2efe0f6ac322436746ad9e7d5e1ddb
maki-engineer <ryohei-81@outlook.jp>
2024-07-21 11:35:14 +0900

    update: main.tsを更新（恐らく完成）

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ export{}
 require('dotenv').config({ path: '.env.development' });
 
 const { DiscordBot } = require('./discord_bot/DiscordBot');
+const { Ready } = require('./discord_bot/ready/Ready');
 const { InteractionCreate } = require('./discord_bot/interaction_create/InteractionCreate');
 const { MessageCreate } = require('./discord_bot/message_create/MessageCreate');
+const { GuildMemberRemove } = require('./discord_bot/guild_member_remove/GuildMemberRemove');
 const discordBot = new DiscordBot(process.env.DISCORD_TOKEN);
+const ready = new Ready(discordBot);
 const interactionCreate = new InteractionCreate(discordBot);
 const messageCreate = new MessageCreate(discordBot);
+const guildMemberRemove = new GuildMemberRemove(discordBot);
 
 
 // 起動
@@ -16,7 +20,16 @@ discordBot.start();
 
 // 常時行う処理
 discordBot.on('ready', () => {
-  console.log('235bot is login now.');
+  // 235botのコマンドを設定
+  ready.setCommand();
+
+  // 235botのステータスを設定
+  ready.setStatus();
+
+  // 1分ごとに処理
+  setInterval(() => {
+    ready.readyEvent();
+  }, 60_000);
 });
 
 // スラッシュコマンドが使われた時に行う処理
@@ -26,3 +39,4 @@ interactionCreate.interactionCreateEvent();
 messageCreate.messageCreateEvent();
 
 // サーバーから誰かが退出した時に行う処理
+guildMemberRemove.guildMemberRemoveEvent();
