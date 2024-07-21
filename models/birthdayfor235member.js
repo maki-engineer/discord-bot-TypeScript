d1b77873eaf7bd048307bac58e3711f8c3934818
maki-engineer <ryohei-81@outlook.jp>
2024-07-21 15:09:36 +0900

    update: モデルメソッドを新たに定義

--- models/birthdayfor235member.js
+++ models/birthdayfor235member.js
@@ -9,8 +9,52 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate(models)
+    {
       // define association here
+    }
+
+    /**
+     * 新しく235プロダクションに入ってきたメンバーの誕生日を登録
+     *
+     * @param {string} userName ユーザー名
+     * @param {string} userId ユーザーID
+     * @param {number} month 月
+     * @param {number} date 日
+     * @param {any} transaction ユニットテストをする時に指定
+     */
+    static async registNew235MemberBirthday(userName, userId, month, date, transaction = null)
+    {
+      const insertData = {};
+
+      if (transaction !== null) {
+        return await this.create(insertData, {transaction});
+      }
+
+      return await this.create(insertData);
+    }
+
+    /**
+     * 指定された235プロダクションメンバーの誕生日を削除
+     *
+     * @param {string} userId メンバーのユーザーID
+     * @param {any} transaction ユニットテストをする時に指定
+     *
+     * @return {object}
+     */
+    static async delete235MemberBirthday(userId, transaction = null)
+    {
+      const deleteData = {
+        where: {
+          user_id: userId
+        }
+      };
+
+      if (transaction !== null) {
+        deleteData.transaction = transaction;
+      }
+
+      return await this.destroy(deleteData);
     }
   }
 
