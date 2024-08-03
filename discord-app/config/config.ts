90d0ca664bd1c743c6caab157c659ae145f35066
maki-engineer <ryohei-81@outlook.jp>
2024-08-03 13:31:40 +0900

    update: 一旦エラーが出るように

--- discord-app/config/config.ts
+++ discord-app/config/config.ts
@@ -5,7 +5,7 @@ export = {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    host: '127.0.0.1',
+    host: 'db',
     port: '5432',
     dialect: 'postgres',
     timezone: '+09:00',
