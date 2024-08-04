7dab4fba0b1a538b989ada73b24add1447405646
maki-engineer <ryohei-81@outlook.jp>
2024-08-04 13:55:05 +0900

    update: DBeaverに接続できるようにするため、ポート番号を変更

--- discord-app/config/config.ts
+++ discord-app/config/config.ts
@@ -6,7 +6,7 @@ export = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     host: 'db',
-    port: '5432',
+    port: '5434',
     dialect: 'postgres',
     timezone: '+09:00',
   },
