cc26fdc656f653ba74d96a347a3e0b3e39fc0b73
maki-engineer <ryohei-81@outlook.jp>
2025-04-08 20:30:56 +0900

    lint-stagedが動くか確認

---
+++ discord-app/lint-staged.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  '*.ts': (files) => {
+    if (files.length === 0) {
+      return [];
+    }
+
+    const formatFileList = files.map((file) => {
+      return path.normalize(path.relative(process.cwd(), file)).replace(/\\/g, '/');
+    });
+
+    const taskList = [];
+
+    // Prettier を実行
+    taskList.push(`npx prettier --write ${formatFileList.join(' ')}`);
+
+    // linter を実行
+    taskList.push(...formatFileList.map((file) => `npm run linter ${file}`));
+
+    const testFileList = formatFileList.filter((file) => file.startsWith('tests/'));
+
+    if (testFileList.length > 0) {
+      // test を実行
+      taskList.push(`npm run test ${testFileList.join(' ')}`);
+    }
+
+    return taskList;
+  },
+};
