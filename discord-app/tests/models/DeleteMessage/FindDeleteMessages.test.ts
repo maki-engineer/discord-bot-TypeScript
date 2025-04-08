89267f31bef21217feab0ec52134aeddca790e2e
maki-engineer <ryohei-81@outlook.jp>
2025-04-08 20:58:28 +0900

    テストが正常に動くか確認

--- discord-app/tests/models/DeleteMessage/FindDeleteMessages.test.ts
+++ discord-app/tests/models/DeleteMessage/FindDeleteMessages.test.ts
@@ -6,38 +6,41 @@ describe('正常系（DeleteMessage.findDeleteMessages）', (): void => {
   let transaction: any;
   const targetDate = 14;
 
-  beforeEach(async (): Promise < void > => {
+  beforeEach(async (): Promise<void> => {
     transaction = await sequelize.transaction();
   });
 
-  afterEach(async (): Promise < void > => {
+  afterEach(async (): Promise<void> => {
     if (transaction) {
       await transaction.rollback();
     }
   });
 
-  afterAll(async (): Promise < void > => {
+  afterAll(async (): Promise<void> => {
     await sequelize.close();
   });
 
-  test('1週間前に投稿されたメッセージ一覧が配列で返ること', async (): Promise < void > => {
+  test('1週間前に投稿されたメッセージ一覧が配列で返ること', async (): Promise<void> => {
     const dummyData = [
       {
-        message_id: '123456789', date: targetDate,
+        message_id: '123456789',
+        date: targetDate,
       },
       {
-        message_id: '987654321', date: targetDate,
+        message_id: '987654321',
+        date: targetDate,
       },
       {
-        message_id: '334455667788', date: 25,
+        message_id: '334455667788',
+        date: 25,
       },
     ];
 
     await DeleteMessage.bulkCreate(dummyData, { transaction });
 
     const result: {
-      message_id: string,
-      date: number,
+      message_id: string;
+      date: number;
     }[] = await DeleteMessage.findDeleteMessages(targetDate, transaction);
 
     expect(result).toHaveLength(2);
@@ -47,13 +50,10 @@ describe('正常系（DeleteMessage.findDeleteMessages）', (): void => {
     expect(result[0].date).toBe(targetDate);
     expect(result[1].message_id).toBe(dummyData[1].message_id);
     expect(result[1].date).toBe(targetDate);
-    expect(result).toEqual([
-      dummyData[0],
-      dummyData[1],
-    ]);
+    expect(result).toEqual([dummyData[0], dummyData[1]]);
   });
 
-  test('削除対象のメッセージが見つからなかった場合は、空配列が返ること', async (): Promise < void > => {
+  test('削除対象のメッセージが見つからなかった場合は、空配列が返ること', async (): Promise<void> => {
     const result: [] = await DeleteMessage.findDeleteMessages(targetDate, transaction);
 
     expect(result).toHaveLength(0);
