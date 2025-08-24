e24ff18a58971f92f3d816294138f3ccfc514beb
maki-engineer <ryohei-81@outlook.jp>
2025-08-24 21:02:41 +0900

    ISSUE-126 DeleteMessageRepositoryTestをリファクタリング

--- discord-app/tests/repositories/DeleteMessageRepository/FindDeleteMessages.test.ts
+++ discord-app/tests/repositories/DeleteMessageRepository/FindDeleteMessages.test.ts
@@ -1,27 +1,28 @@
-process.env.NODE_ENV = 'unittest';
+import { Transaction } from 'sequelize';
+import DeleteMessageRepository from '../../../repositories/DeleteMessageRepository';
+import db from '../../../models/index';
 
-const DeleteMessageRepository = require('../../../repositories/DeleteMessageRepository').default;
-const { DeleteMessage, sequelize } = require('../../../models/index').default;
+const { DeleteMessage, sequelize } = db;
 
-describe('正常系（findDeleteMessages）', (): void => {
-  let transaction: any;
+describe('正常系（findDeleteMessages）', () => {
+  let transaction: Transaction;
   const targetDate = 14;
 
-  beforeEach(async (): Promise<void> => {
-    transaction = await sequelize.transaction();
+  beforeEach(async () => {
+    transaction = await sequelize!.transaction();
   });
 
-  afterEach(async (): Promise<void> => {
+  afterEach(async () => {
     if (transaction) {
       await transaction.rollback();
     }
   });
 
-  afterAll(async (): Promise<void> => {
-    await sequelize.close();
+  afterAll(async () => {
+    await sequelize!.close();
   });
 
-  test('1週間前に投稿されたメッセージ一覧が配列で返ること', async (): Promise<void> => {
+  test('1週間前に投稿されたメッセージ一覧が配列で返ること', async () => {
     const dummyData = [
       {
         message_id: '123456789',
@@ -39,10 +40,7 @@ describe('正常系（findDeleteMessages）', (): void => {
 
     await DeleteMessage.bulkCreate(dummyData, { transaction });
 
-    const result: {
-      message_id: string;
-      date: number;
-    }[] = await DeleteMessageRepository.findDeleteMessages(targetDate, transaction);
+    const result = await DeleteMessageRepository.findDeleteMessages(targetDate, transaction);
 
     expect(result).toHaveLength(2);
     expect(result).toBeInstanceOf(Array);
@@ -54,8 +52,8 @@ describe('正常系（findDeleteMessages）', (): void => {
     expect(result).toEqual([dummyData[0], dummyData[1]]);
   });
 
-  test('削除対象のメッセージが見つからなかった場合は、空配列が返ること', async (): Promise<void> => {
-    const result: [] = await DeleteMessageRepository.findDeleteMessages(targetDate, transaction);
+  test('削除対象のメッセージが見つからなかった場合は、空配列が返ること', async () => {
+    const result = await DeleteMessageRepository.findDeleteMessages(targetDate, transaction);
 
     expect(result).toHaveLength(0);
     expect(result).toEqual([]);
