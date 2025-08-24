fcbc87fb30a966c338fc6d02e945b2b375ffcfcf
maki-engineer <ryohei-81@outlook.jp>
2025-08-24 20:56:02 +0900

    ISSUE-126 DictWordRepositoryTestをリファクタリング

--- discord-app/tests/repositories/DictWordRepository/UpdateReadOfWordToDict.test.ts
+++ discord-app/tests/repositories/DictWordRepository/UpdateReadOfWordToDict.test.ts
@@ -1,30 +1,29 @@
-process.env.NODE_ENV = 'unittest';
+import { Transaction } from 'sequelize';
+import DictWordRepository from '../../../repositories/DictWordRepository';
+import db from '../../../models/index';
 
-require('dotenv').config();
+const { DictWord, sequelize } = db;
 
-const DictWordRepository = require('../../../repositories/DictWordRepository').default;
-const { DictWord, sequelize } = require('../../../models/index').default;
-
-describe('正常系（updateReadOfWordToDict）', (): void => {
-  let transaction: any;
+describe('正常系（updateReadOfWordToDict）', () => {
+  let transaction: Transaction;
   const targetWord = 'test';
   const setHowToRead = 'テスト';
 
-  beforeEach(async (): Promise<void> => {
-    transaction = await sequelize.transaction();
+  beforeEach(async () => {
+    transaction = await sequelize!.transaction();
   });
 
-  afterEach(async (): Promise<void> => {
+  afterEach(async () => {
     if (transaction) {
       await transaction.rollback();
     }
   });
 
-  afterAll(async (): Promise<void> => {
-    await sequelize.close();
+  afterAll(async () => {
+    await sequelize!.close();
   });
 
-  test('入力された単語が単語辞書に登録されていた場合は、正常に読み方が更新されること', async (): Promise<void> => {
+  test('入力された単語が単語辞書に登録されていた場合は、正常に読み方が更新されること', async () => {
     const dummyData = {
       word: targetWord,
       how_to_read: 'ティーイ―エスティ―',
@@ -32,10 +31,7 @@ describe('正常系（updateReadOfWordToDict）', (): void => {
 
     await DictWord.create(dummyData, { transaction });
 
-    let dictWordData: {
-      word: string;
-      how_to_read: string;
-    }[] = await DictWord.findAll({ raw: true, transaction });
+    let dictWordData = await DictWord.findAll({ raw: true, transaction });
 
     expect(dictWordData[0].how_to_read).not.toBe(setHowToRead);
 
@@ -46,7 +42,7 @@ describe('正常系（updateReadOfWordToDict）', (): void => {
     expect(dictWordData[0].how_to_read).toBe(setHowToRead);
   });
 
-  test('入力された単語が単語辞書に登録されていなかった場合は、読み方が更新されないこと', async (): Promise<void> => {
+  test('入力された単語が単語辞書に登録されていなかった場合は、読み方が更新されないこと', async () => {
     const dummyData = {
       word: '235',
       how_to_read: 'フミコ',
@@ -54,10 +50,7 @@ describe('正常系（updateReadOfWordToDict）', (): void => {
 
     await DictWord.create(dummyData, { transaction });
 
-    let dictWordData: {
-      word: string;
-      how_to_read: string;
-    }[] = await DictWord.findAll({ raw: true, transaction });
+    let dictWordData = await DictWord.findAll({ raw: true, transaction });
 
     expect(dictWordData[0].how_to_read).not.toBe(setHowToRead);
 
