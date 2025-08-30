168db0e799f101b6641b66a2eb771f1bab634136
maki-engineer <ryohei-81@outlook.jp>
2025-08-30 20:25:44 +0900

    fix 235botのメンション以外には反応しないように修正

--- discord-app/src/discord_bot/message_create/deleteReplyMessage.ts
+++ discord-app/src/discord_bot/message_create/deleteReplyMessage.ts
@@ -1,12 +1,12 @@
 import { Message } from 'discord.js';
 
 /**
- * 235botのメッセージがリプライだった場合、1分後に削除
+ * 235botのメッセージがリプライだった場合、5分後に削除
  *
  * @param {Message} message Messageクラス
  */
 export default (message: Message) => {
   if (message.author.bot && message.mentions.repliedUser) {
-    setTimeout(() => message.delete(), 60_000);
+    setTimeout(() => message.delete(), 300_000);
   }
 };
